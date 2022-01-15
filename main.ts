2efe0386736be335fae04665f9a271a3dc0d5440
Jiří Slíva <jiri.sliva@gmail.com>
2022-01-15 14:48:42 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+function text_na_dobu (d: string) {
+    if (d == "1") {
+        return music.beat(BeatFraction.Whole)
+    } else if (d == "2") {
+        return music.beat(BeatFraction.Half)
+    } else if (d == "4") {
+        return music.beat(BeatFraction.Quarter)
+    } else if (d == "8") {
+        return music.beat(BeatFraction.Eighth)
+    } else {
+        return music.beat(BeatFraction.Whole)
+    }
+}
 function hrej_notu (nota: string) {
     ton_a_doba = nota.split(" ")
     ton = ton_a_doba[0]
@@ -18,58 +31,67 @@ function hrej_notu (nota: string) {
         music.playTone(494, doba)
     } else if (ton == "C2") {
         music.playTone(523, doba)
-    } else if (ton == "P") {
-        music.rest(doba)
     }
+    music.rest(music.beat(BeatFraction.Eighth))
 }
 input.onButtonPressed(Button.A, function () {
     hrej(ovecka)
 })
-function hrej (noty: any[]) {
-    for (let hodnota of noty) {
-        hrej_notu(hodnota)
-    }
-}
-function text_na_dobu (d: string) {
-    if (d == "1") {
-        return music.beat(BeatFraction.Whole)
-    } else if (d == "2") {
-        return music.beat(BeatFraction.Half)
-    } else if (d == "4") {
-        return music.beat(BeatFraction.Quarter)
-    } else if (d == "8") {
-        return music.beat(BeatFraction.Eighth)
-    } else {
-        return music.beat(BeatFraction.Whole)
-    }
-}
 input.onButtonPressed(Button.B, function () {
     hrej(Sluníčko)
 })
 let doba = 0
 let ton = ""
 let ton_a_doba: string[] = []
-let Sluníčko: string[] = []
 let ovecka: string[] = []
+let Sluníčko: string[] = []
+function hrej (noty: any[]) {
+    for (let hodnota of noty) {
+        hrej_notu(hodnota)
+    }
+}
 music.setVolume(179)
 music.setTempo(90)
 ovecka = [
 "G1 4",
-"P 8",
+"",
 "A1 4",
 "P 8",
 "H1 2",
 "P 4",
-"C1 2",
-"P 8",
-"C1 4",
-"P 8",
-"C1 4",
+"C2 2",
 "P 4",
-"C1 4",
+"C2 4",
+"P 8",
+"C2 4",
 "P 4",
+"C2 4",
+"P 8",
 "H1 4",
+"P 8",
+"A1 2",
+"P 8",
+"H1 4",
+"P 8",
+"A1 4",
+"P 8",
+"G1 2",
 "P 4",
-"A1 2"
+"A1 2",
+"P 4",
+"A1 4",
+"P 8",
+"A1 4",
+"P 4",
+"A1 4",
+"P 8",
+"H1 4",
+"A1 2",
+"P 4",
+"G1 2",
+"P 8",
+"G1 4",
+"P 8",
+"G1 4"
 ]
 Sluníčko = ["A1 8"]
